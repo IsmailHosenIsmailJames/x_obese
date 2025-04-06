c720140bb0cf0d4eda16a03b8cccfb7f77d4083f
Ismail Hosen James <md.ismailhosenismailjames@gmail.com>
2025-04-06 19:40:57 +0600

    [removed] removed background fetch

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:background_fetch/background_fetch.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_foreground_task/flutter_foreground_task.dart';
@@ -8,26 +7,6 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:o_xbese/o_xbese.dart';
 import 'package:o_xbese/src/apis/middleware/jwt_middleware.dart';
 import 'package:o_xbese/src/screens/auth/controller/auth_controller.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
-@pragma('vm:entry-point')
-void backgroundFetchHeadlessTask(HeadlessTask task) async {
-  String taskId = task.taskId;
-  bool isTimeout = task.timeout;
-  if (isTimeout) {
-    // This task has exceeded its allowed running-time.
-    // You must stop what you're doing and immediately .finish(taskId)
-    print("[BackgroundFetch] Headless task timed-out: $taskId");
-    SharedPreferences preferences = await SharedPreferences.getInstance();
-    int count = preferences.getInt('task') ?? 0;
-    await preferences.setInt('task', count + 1);
-    BackgroundFetch.finish(taskId);
-    return;
-  }
-  print('[BackgroundFetch] Headless event received.');
-  // Do your work here...
-  BackgroundFetch.finish(taskId);
-}
 
 Future<void> main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
@@ -48,8 +27,6 @@ Future<void> main() async {
   final authController = Get.put(AuthController());
   authController.refreshToken.value = getRefreshToken();
   authController.accessToken.value = getAccessToken();
-
-  await BackgroundFetch.registerHeadlessTask(backgroundFetchHeadlessTask);
 
   runApp(const OXbese());
 }
--- lib/src/screens/navs/naves_page.dart
+++ lib/src/screens/navs/naves_page.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:developer';
 import 'dart:io';
 
-import 'package:background_fetch/background_fetch.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_foreground_task/flutter_foreground_task.dart';
 import 'package:flutter_svg/svg.dart';
@@ -124,7 +123,6 @@ class _NavesPageState extends State<NavesPage> {
       // Request permissions and initialize the service.
 
       await _requestPermissions();
-      await initPlatformState();
       _initService();
       await _startService();
     });
@@ -135,58 +133,6 @@ class _NavesPageState extends State<NavesPage> {
   @override
   void dispose() {
     super.dispose();
-  }
-
-  // background fetch
-  bool _enabled = true;
-  int _status = 0;
-  List<DateTime> _events = [];
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    // Configure BackgroundFetch.
-    int status = await BackgroundFetch.configure(
-      BackgroundFetchConfig(
-        minimumFetchInterval: 15,
-        stopOnTerminate: false,
-        enableHeadless: true,
-        requiresBatteryNotLow: false,
-        requiresCharging: false,
-        requiresStorageNotLow: false,
-        requiresDeviceIdle: false,
-        requiredNetworkType: NetworkType.ANY,
-        startOnBoot: true,
-      ),
-      (String taskId) async {
-        // <-- Event handler
-        // This is the fetch-event callback.
-        print("[BackgroundFetch] Event received $taskId");
-        setState(() {
-          _events.insert(0, new DateTime.now());
-        });
-        SharedPreferences preferences = await SharedPreferences.getInstance();
-        int count = preferences.getInt('task1') ?? 0;
-        await preferences.setInt('task1', count + 1);
-        // IMPORTANT:  You must signal completion of your task or the OS can punish your app
-        // for taking too long in the background.
-        BackgroundFetch.finish(taskId);
-      },
-      (String taskId) async {
-        // <-- Task timeout handler.
-        // This task has exceeded its allowed running-time.  You must stop what you're doing and immediately .finish(taskId)
-        print("[BackgroundFetch] TASK TIMEOUT taskId: $taskId");
-        BackgroundFetch.finish(taskId);
-      },
-    );
-    print('[BackgroundFetch] configure success: $status');
-    setState(() {
-      _status = status;
-    });
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
   }
 
   @override
