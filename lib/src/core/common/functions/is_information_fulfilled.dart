561d640ea402bee35871c2b047116113665aacec
MD. Ismail Hosen James <md.ismailhosenismailjames@gmail.com>
2025-04-30 10:56:06 +0600

    [fix] image is not mandatory for a profile

--- lib/src/core/common/functions/is_information_fulfilled.dart
+++ lib/src/core/common/functions/is_information_fulfilled.dart
@@ -1,8 +1,7 @@
 import 'package:x_obese/src/screens/controller/info_collector/model/all_info_model.dart';
 
 bool isInformationNotFullFilled(AllInfoModel userData) {
-  return userData.image == null ||
-      userData.fullName == null ||
+  return userData.fullName == null ||
       userData.email == null ||
       userData.address == null ||
       userData.birth == null ||
