5c885872268efc732587bd79d1db5bbbcf790079
MD. Ismail Hosen <md.ismailhosenismailjames@gmail.com>
2026-03-26 11:04:10 +0600

    feat: Update Android NDK version, remove `open_file_ios` pod, and upgrade various Flutter, Android, and iOS dependencies.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.impala.x_obese"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "29.0.14206865"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
